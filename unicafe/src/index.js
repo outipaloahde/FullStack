bffd8c251b4d9289d10d6803107b670f27001a77
outipaloahde <outipaloahde@gmail.com>
2018-07-27 11:44:17 +0300

    1.8

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,93 +1,134 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
+const Button = props => {
+  return (
+    <div>
+      <button onClick={props.funktio}>{props.text}</button>
+    </div>
+  );
+};
 
-const Stats = (props) =>{
-    const hyvaKpl = props.hyva 
-    const neutKpl = props.neutraali 
-    const huonoKpl = props.huono
-    const yht = hyvaKpl + neutKpl + huonoKpl
-    
-    const ka = hyvaKpl 
-        return (
-            <div>
-                <p>ka={ka}</p>
-            </div>
-        )
+const Statistics = props => {
+  return (
+    <div>
+      <h3>Statistiikka</h3>
+      <table>
+        <Statistic arvostelut={props.arvostelut} />
+      </table>
+    </div>
+  );
+};
+
+const Statistic = props => (
+  <tbody>
+    <tr>
+      <td>hyvä</td>
+      <td />
+      <td>{props.arvostelut[0]}</td>
+    </tr>
+    <tr>
+      <td>neutraali</td>
+      <td />
+      <td>{props.arvostelut[1]}</td>
+    </tr>
+    <tr>
+      <td>huono</td>
+      <td />
+      <td>{props.arvostelut[2]}</td>
+    </tr>
+    <tr>
+      <Average arvostelut={props.arvostelut} />
+      <Percent arvostelut={props.arvostelut} />
+    </tr>
+  </tbody>
+);
+
+const Average = props => {
+  let hyva = props.arvostelut[0];
+  let huono = props.arvostelut[2];
+  let yht = props.arvostelut[3];
+  let ka = (hyva * 1 + huono * -1) / yht;
+  return (
+    <div>
+      <p>Keskiarvo {parseFloat(ka).toFixed(1)}</p>
+    </div>
+  );
+};
+const Percent = props => {
+  let hyva = props.arvostelut[0];
+  let yht = props.arvostelut[3];
+  let perc = (hyva / yht) * 100;
+  return (
+    <div>
+      <p>Positiivista {parseFloat(perc).toFixed(0)}%</p>
+    </div>
+  );
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hyva: 0,
+      neutraali: 0,
+      huono: 0,
+      yht: 0
+    };
+  }
+
+  klikHyva = () => {
+    this.setState({
+      hyva: this.state.hyva + 1,
+      yht: this.state.yht + 1
+    });
+  };
+  klikNeutraali = () => {
+    this.setState({
+      neutraali: this.state.neutraali + 1,
+      yht: this.state.yht + 1
+    });
+  };
+  klikHuono = () => {
+    this.setState({
+      huono: this.state.huono + 1,
+      yht: this.state.yht + 1
+    });
+  };
+  render() {
+    if (
+      this.state.hyva === 0 &&
+      this.state.huono === 0 &&
+      this.state.neutraali === 0
+    ) {
+      return (
+        <div>
+          <h2>Anna Palautetta</h2>
+          <Button text="hyvä" funktio={this.klikHyva} />
+          <Button text="neutraali" funktio={this.klikNeutraali} />
+          <Button text="huono" funktio={this.klikHuono} />
+          <p>Palautetta ei ole vielä annettu</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h2>Anna Palautetta</h2>
+          <Button text="hyvä" funktio={this.klikHyva} />
+          <Button text="neutraali" funktio={this.klikNeutraali} />
+          <Button text="huono" funktio={this.klikHuono} />
+          <Statistics
+            arvostelut={[
+              this.state.hyva,
+              this.state.neutraali,
+              this.state.huono,
+              this.state.yht
+            ]}
+          />
+        </div>
+      );
+    }
+  }
 }
 
-class App extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            hyva:0,
-            neutraali:0,
-            huono:0,
-            kaikki: [],
-            yht:0,
-        }
-    }
-
-
-
-    klikHyva = () => {
-        this.setState({
-            hyva: this.state.hyva + 1,
-            kaikki: this.state.kaikki.concat('hy'),
-            yht: this.state.yht + 1
-        })
-    }
-    klikNeutraali = () => {
-        this.setState({
-            neutraali: this.state.neutraali + 1,
-            kaikki: this.state.kaikki.concat('ne'),
-            yht: this.state.yht + 1
-        })
-    }
-    klikHuono = () => {
-        this.setState({
-            huono: this.state.huono + 1,
-            kaikki: this.state.kaikki.concat('hu'),
-            yht: this.state.yht + 1
-        })
-    }
-
-    countStats = () => {
-        let hyva = this.state.hyva
-        let huono = this.state.huono
-        let neutraali = this.state.neutraali
-        let yht = this.state.yht
-        let ka = ((hyva * 1)+(huono*(-1))) / yht
-        return ka
-    }
-    countPercent = () => {
-        let hyva = this.state.hyva
-        let yht = this.state.yht
-        let perc = hyva / yht * 100
-        return perc
-    }
-
-    render() {
-        let keskiarvo = this.countStats();
-        let percentage = this.countPercent();
-        
-        return (
-            <div>
-                <h2>Anna Palautetta</h2>
-                <button onClick={this.klikHyva}>Hyvä</button>
-                <button onClick={this.klikNeutraali}>Neutraali</button>
-                <button onClick={this.klikHuono}>Huono</button>
-                <h3>Statistiikka</h3>
-                <p>Hyvä {this.state.hyva} kpl<br />
-                Neutraali {this.state.neutraali} kpl<br />
-                Huono {this.state.huono} kpl</p>
-                <p>Palautteita yhteensä {this.state.yht}</p>
-                <p>Keskiarvo {parseFloat(keskiarvo).toFixed(1)} Positiivista {parseFloat(percentage).toFixed(1)}%</p>
-
-            </div>
-        )
-    }
-}
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
